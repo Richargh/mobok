84763f6e500be1aa18a312fbaebc9a13be2fdf47
Richard Gross <richard.gross@maibornwolff.de>
2020-03-22 11:19:21 +0100

    r introduce git result

--- src/main/kotlin/de/richargh/mobok/GitAdapter.kt
+++ src/main/kotlin/de/richargh/mobok/GitAdapter.kt
@@ -5,35 +5,32 @@ import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 
-class GitAdapter(val baseFolder: File = File(".")){
-    fun git(vararg arguments: String){
+class GitAdapter(val baseFolder: File = File(".")) {
+
+    fun git(vararg arguments: String): GitResult {
         val os = System.getProperty("os.name")
-        val processBuilder = if (os.toLowerCase().contains("mac")) {
-            println("We're on Mac => [$os]")
-            ProcessBuilder("bash", "-c", "git ${arguments.joinToString(" ")}")
-        } else if (os.toLowerCase().contains("win")) {
-            println("We're on Windows => [$os]")
-            ProcessBuilder("cmd.exe", "/c", "git ${arguments.joinToString(" ")}")
-        } else {
-            println("OS is unknown => [$os]")
-            return
+        val processBuilder = when {
+            os.toLowerCase().contains("mac") ->
+                ProcessBuilder("bash", "-c", "git ${arguments.joinToString(" ")}")
+            os.toLowerCase().contains("win") ->
+                ProcessBuilder("cmd.exe", "/c", "git ${arguments.joinToString(" ")}")
+            else                             ->
+                return GitResult(CliCode.UNKNOWN_OS, emptyList())
         }
         processBuilder.directory(baseFolder)
 
-        try {
+        return try {
             val process = processBuilder.start()
             val reader = BufferedReader(InputStreamReader(process.inputStream))
-            reader.useLines { lines ->
-                println(lines.joinToString("\n"))
-            }
+            val lines: List<String> = reader.useLines { it }.toList()
             val exitCode = process.waitFor()
-            if (exitCode != 0) {
-                println("\nSomething went wrong, the exit code was: [$exitCode]")
-            }
+            GitResult(CliCode.ofExitCode(exitCode), lines)
         } catch (e: IOException) {
             e.printStackTrace()
+            GitResult(CliCode.EXCEPTION, listOf(e.toString()))
         } catch (e: InterruptedException) {
             e.printStackTrace()
+            GitResult(CliCode.EXCEPTION, listOf(e.toString()))
         }
     }
 }
---
+++ src/main/kotlin/de/richargh/mobok/GitResult.kt
@@ -0,0 +1,18 @@
+package de.richargh.mobok
+
+data class GitResult(val code: CliCode, val message: List<String>)
+
+sealed class CliCode {
+    object UNKNOWN_OS: CliCode()
+    object EXCEPTION: CliCode()
+
+    object OK: CliCode()
+    data class OTHER(val rawValue: Int): CliCode()
+
+    companion object {
+        fun ofExitCode(exitCode: Int) = when(exitCode){
+            0 -> OK
+            else -> OTHER(exitCode)
+        }
+    }
+}
