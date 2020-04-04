aefabd0290cd27b105d13da7c96f673f6af9fa7e
Richard Gross <richard.gross@maibornwolff.de>
2020-04-04 08:12:42 +0200

    F replace config

--- src/main/kotlin/de/richargh/mobok/ConfigVM.kt
+++ src/main/kotlin/de/richargh/mobok/ConfigVM.kt
@@ -8,32 +8,5 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 class ConfigVM: ViewModel() {
-    override var configPath = Paths.get("..", "no.config")
 
-    private val events: Events by inject()
-
-    private val settingsFile = SimpleObjectProperty<Path>().apply {
-        onChange(::changeConfigPath)
-        set(Paths.get("..", "mobok.config"))
-    }
-
-    val settings = settingsFile.stringBinding { it?.normalize()?.toAbsolutePath()?.toString() ?: "" }
-
-    private fun changeConfigPath(newPath: Path?) {
-        println("Changing config from [$configPath] to [$newPath]")
-        configPath = newPath
-        if (newPath != null)
-            initConfig()
-    }
-
-    private fun initConfig(){
-        config.double("Foo") ?: config.set("Foo" to 2)
-        config.save()
-
-        //        events.configChanged.push()
-    }
-
-    fun changeSettingsFile(settingsPath: Path?) {
-        settingsFile.set(settingsPath)
-    }
 }
--- src/main/kotlin/de/richargh/mobok/ConfigView.kt
+++ src/main/kotlin/de/richargh/mobok/ConfigView.kt
@@ -1,5 +1,7 @@
 package de.richargh.mobok
 
+import com.sun.javafx.binding.BidirectionalBinding.bind
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.geometry.Pos
 import javafx.stage.FileChooser
 import tornadofx.*
@@ -8,17 +10,30 @@ class ConfigView: View() {
 
     private val configVM: ConfigVM by inject()
 
-    override val root = borderpane {
-        left = label("Settings") { alignment = Pos.CENTER }
-        center = textfield(configVM.settings) { isEditable = false }
-        right = button("Select ") {
-            action {
-                val dir = chooseFile(
-                        "Select Properties",
-                        arrayOf(FileChooser.ExtensionFilter("Properties", "*.config")))
-                configVM.changeSettingsFile(dir.firstOrNull()?.toPath())
+    private val count = SimpleIntegerProperty()
+    private val name = count.stringBinding { "History ($it)" }
 
+    override val root = borderpane {
+        left = vbox(alignment = Pos.TOP_CENTER) { button("Open") }
+        center = hbox(alignment = Pos.CENTER) {
+            form {
+                fieldset {
+                    field("Project") { label("Foo") }
+                    field("Branch") { label("Bar") }
+                    field("Branch") { label("Bar") }
+                }
             }
+        }
+        right = vbox(alignment = Pos.TOP_CENTER) { button(name) { } }
+    }
+
+    private fun prefs(){
+        var bool: Boolean = false
+        var str: String = ""
+        preferences("test app") {
+            putBoolean("boolean", true)
+            bool = getBoolean("boolean key", false)
+            str = get("string", "")
         }
     }
 }
